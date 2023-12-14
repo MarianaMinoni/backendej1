614dd066115997ab43e6effe5d9c6f98cb929781
Mariana Minoni <marianaminoni@gmail.com>
2023-12-14 12:23:41 -0300

    primer desafio

--- desafio1.js
+++ desafio1.js
@@ -1,37 +1,68 @@
-// hacer una clase ticketmanager.. la misma es una "coleccion" 
+// hacer una clase ticketmanager
 
 class ProductManager {
-    eventos = [];
-    precioBaseDeGanancia = 500;
-    id = 0;
+    constructor(){
+        this.productos = [];
+        this.id = 0;
+
+    }
+  
 
 
-    getEventos(){
-        return this.eventos
+    getProductos(){
+        return this.productos;
     }
 
-    //algoritmo mas básico
+    getProductbyId(productId){
+        let producto = this.productos.find(producto => producto.id === productId)
+        
+        if (producto) {
+            return producto
+        } else {
+            console.log('Not found');
+        }
+    }
 
-    agregarEvento(nombre, lugar, precio, capacidad){
-        let evento = {
+// metodo agregar producto con las validaciones
+    addproduct(title, description, price, thumbnail, code, stock){
+
+        if (!title || !description || !price || !thumbnail || !code || !stock){
+            console.log('todos los datos son obligatorios')
+        }
+
+        let codeExist = this.productos.some(producto => producto.code === code);
+
+        if(codeExist){
+            console.log('el codigo ingresado es incorrecto')
+        } else{
+
+        
+        let producto = {
             id:this.id,
-            nombre:nombre,
-            lugar:lugar,
-            precio:precio,
-            capacidad:capacidad,
-            fecha:fecha,
-            participantes:[]
+            title:title,
+            description:description,
+            price:price,
+            thumbnail: thumbnail,
+            code:code,
+            stock:stock,
     
         }
-        this.eventos.push(evento)
-        this.id = this.id ++;
+        this.productos.push(producto);
+        this.id ++;
 
-
+    }
     }
 
 
 
 }
 
-ProductManager()
+const productManager = new ProductManager();
 
+
+productManager.addproduct('t-shirt', 'white t-shirt', '100', 'img1.jpeg','1100', '10');
+productManager.addproduct('shoe', 'sports shoes', '250', 'img2.jpeg','1101', '5');
+
+
+console.log(productManager.getProductos());
+console.log(productManager.getProductbyId(4))
